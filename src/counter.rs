321efd08d9b876279c45e2309590c0314bf9078b
Marijon Pierre <pierre.marijon@inria.fr>
2019-02-19 16:13:50 +0100

    Try to avoid polluted cache with bucket

--- src/counter.rs
+++ src/counter.rs
@@ -39,7 +39,7 @@ impl Inc<u16> for IncUnsigned {
 }
 
 pub trait Counter<CounterType, BucketId, KmerType> {
-    fn incs(&mut self, bucket_id: BucketId, bucket: Vec<KmerType>);
+    fn incs(&mut self, bucket_id: BucketId, bucket: NoTemporalArray);
     fn get(&self, kmer: KmerType) -> CounterType;
 }
 
@@ -64,9 +64,9 @@ macro_rules! impl_basiccounter {
 macro_rules! impl_counter_for_basiccounter {
     ($type:ty) => (
         impl Counter<$type, u64, u64> for BasicCounter<$type> {
-            fn incs(&mut self, _bucket_id: u64, bucket: Vec<u64>) {
-                for i in bucket {
-                    self.incrementor.inc(&mut self.data[i as usize]);
+            fn incs(&mut self, _bucket_id: u64, bucket: NoTemporalArray) {
+                for i in &bucket {
+                    self.incrementor.inc(&mut self.data[*i as usize]);
                 }
             }
             
@@ -82,22 +82,57 @@ impl_basiccounter!(u16);
 impl_counter_for_basiccounter!(u8);
 impl_counter_for_basiccounter!(u16);
 
+const BUCKET_SIZE: usize = 4096;
+
+#[derive(Copy, Clone)]
+pub struct NoTemporalArray {
+    data: [u64; BUCKET_SIZE],
+    pos: usize
+}
+
+impl NoTemporalArray {
+    fn new() -> Self {
+        NoTemporalArray {
+            data: [0; BUCKET_SIZE],
+            pos: 0
+        }
+    }
+
+    fn push(&mut self, val: u64) -> () {
+        unsafe { core::arch::x86_64::_mm_stream_pi(self.data.as_mut_ptr().add(self.pos) as *mut std::arch::x86_64::__m64, std::mem::transmute(val)) }
+        self.pos += 1
+    }
+    
+    fn len(&self) -> usize {
+        self.pos
+    }    
+}
+
+impl<'a> std::iter::IntoIterator for &'a NoTemporalArray {
+    type Item = &'a u64;
+    type IntoIter = std::slice::Iter<'a, u64>;
+
+    fn into_iter(self) -> std::slice::Iter<'a, u64> {
+        self.data.iter()
+    }
+}
+
+
 pub struct Bucketizer<'a, T> {
     pub counter: &'a mut Counter<T, u64, u64>,
-    buckets: Vec<Vec<u64>>,
+    buckets: Vec<NoTemporalArray>,
     k: u8,
     bucket_size: usize,
 }
 
 impl<'a, T> Bucketizer<'a, T> {
     pub fn new(counter: &'a mut Counter<T, u64, u64>, k: u8) -> Self {
-        let bucket_size: usize = 1024;
 
         Bucketizer {
             counter: counter,
-            buckets: vec![Vec::with_capacity(bucket_size); nb_bucket(k)],
+            buckets: vec![NoTemporalArray::new(); nb_bucket(k)],
             k: k,
-            bucket_size: bucket_size,
+            bucket_size: BUCKET_SIZE,
         }
     }
 
@@ -119,7 +154,7 @@ impl<'a, T> Bucketizer<'a, T> {
 
     fn clean_bucket(&mut self, prefix: usize, new_size: usize) -> () {
         self.counter.incs(prefix as u64,
-            std::mem::replace(&mut self.buckets[prefix], Vec::with_capacity(new_size))
+                          std::mem::replace(&mut self.buckets[prefix], NoTemporalArray::new())
         );
     }
 
