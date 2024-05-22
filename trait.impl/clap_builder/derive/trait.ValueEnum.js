1f4f1ab5c169e7e1361b1425777834c4d5c12de1
natir <natir@users.noreply.github.com>
2024-05-22 14:10:52 +0000

    deploy: 4584f753b7289f891733bd925311d5cbdd4af6a1

--- trait.impl/clap_builder/derive/trait.ValueEnum.js
+++ trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"pcon":[["impl ValueEnum for <a class=\"enum\" href=\"pcon/cli/enum.DumpType.html\" title=\"enum pcon::cli::DumpType\">DumpType</a>"]]
+"pcon":[["impl ValueEnum for <a class=\"enum\" href=\"pcon/cli/enum.DumpType.html\" title=\"enum pcon::cli::DumpType\">DumpType</a>"],["impl ValueEnum for <a class=\"enum\" href=\"pcon/cli/enum.Format.html\" title=\"enum pcon::cli::Format\">Format</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
